79f988cf50354b956a83db134c0db5218c83e340
Jesus Molina <netod90@gmail.com>
2023-09-05 16:51:41 -0700

    Add second api call for images

--- projects/04-prueba-tecnica/README.md
+++ projects/04-prueba-tecnica/README.md
@@ -5,6 +5,6 @@ Apis:
 -Imagen random: https://cataas.com/cat/says/hello
 
 - Recupera un hecho aleatorio de gatos de la primera API
-- Recuperar la primera palabra del hecho
+- Recuperar la primera palabra del hecho (fact)
 - Muestra una imagen de un gato con la primera palabra.
   y muestra una imagen de un gato con la primera palabra del hecho recuperado ysando la segunda API.
--- projects/04-prueba-tecnica/src/App.jsx
+++ projects/04-prueba-tecnica/src/App.jsx
@@ -1,18 +1,45 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
 
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
+//  const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}`;
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com';
+
 function App() {
-  const [fact, setFact] = useState('lorem ipsum cat fact');
+  const [fact, setFact] = useState();
+  const [imageUrl, setImageUrl] = useState();
+  const completeUrl = `${CAT_PREFIX_IMAGE_URL}${imageUrl}`;
 
   useEffect(() => {
-    fetch('https://catfact.ninja/fact')
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
       .then((response) => response.json())
-      .then((data) => setFact(data.fact));
+      .then((data) => {
+        const { fact } = data;
+        setFact(fact);
+
+        const firstWord = fact.split(' ')[0];
+        console.log(firstWord);
+
+        fetch(
+          `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`,
+        )
+          .then((res) => res.json())
+          .then((response) => {
+            const { url } = response;
+            setImageUrl(url);
+          });
+      });
   }, []);
   return (
     <main>
       <h1>Cat App</h1>
-      <p>{fact}</p>
+      {fact && <p>{fact}</p>}
+      {imageUrl && (
+        <img
+          src={completeUrl}
+          alt={`Image extracting using the first word for ${fact}`}
+        />
+      )}
     </main>
   );
 }
