a32be9b4a01ab63d3c382f2b6a789122ca91fe8e
Jesus Molina <netod90@gmail.com>
2023-09-06 12:05:00 -0700

    Add file for hook useCatFact

--- projects/04-prueba-tecnica/src/App.jsx
+++ projects/04-prueba-tecnica/src/App.jsx
@@ -1,22 +1,9 @@
-import { useEffect, useState } from 'react';
 import './App.css';
-import { getRandomFact } from './services/facts';
+import { useCatFact } from './hooks/useCatFact';
 import { useCatImage } from './hooks/useCatImage';
 
 //  const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}`;
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com';
-
-const useCatFact = () => {
-  const [fact, setFact] = useState();
-
-  const refreshFact = () => {
-    getRandomFact().then((newFact) => setFact(newFact)); //  getRandomFact().then(setFact) < equivalent
-  };
-  //  GET FACT
-  useEffect(refreshFact, []);
-
-  return { fact, refreshFact };
-};
 
 function App() {
   const { fact, refreshFact } = useCatFact();
