436ea7b95d76eda14231e2e99d426992353ca2fd
Jesus Molina <netod90@gmail.com>
2023-09-05 17:19:30 -0700

    Add style

--- projects/04-prueba-tecnica/src/App.jsx
+++ projects/04-prueba-tecnica/src/App.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import './App.css';
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact';
 //  const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}`;
@@ -10,26 +10,34 @@ function App() {
   const [imageUrl, setImageUrl] = useState();
   const completeUrl = `${CAT_PREFIX_IMAGE_URL}${imageUrl}`;
 
+  //  Get fact on load
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then((response) => response.json())
+      .then((res) => {
+        if (!res.ok) throw new Error('Error fetching fact'); //  if res not ok
+        return res.json();
+      })
       .then((data) => {
         const { fact } = data;
         setFact(fact);
-
-        const firstWord = fact.split(' ')[0];
-        console.log(firstWord);
-
-        fetch(
-          `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`,
-        )
-          .then((res) => res.json())
-          .then((response) => {
-            const { url } = response;
-            setImageUrl(url);
-          });
       });
   }, []);
+
+  //  get image with each new fact
+  useEffect(() => {
+    if (!fact) return; // if there is not fact return
+    const firstWord = fact.split(' ')[0];
+
+    fetch(
+      `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`,
+    )
+      .then((res) => res.json())
+      .then((response) => {
+        const { url } = response;
+        setImageUrl(url);
+      });
+  }, [fact]);
+
   return (
     <main>
       <h1>Cat App</h1>
