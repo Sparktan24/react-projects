e05084c48331d4343932e743557867cc41cac7be
Jesus Molina <netod90@gmail.com>
2023-09-06 18:48:37 -0700

    Add Basic structure & Api mock

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css';
+import responseMovies from './mocks/with-results.json';
+import noResponseMovies from './mocks/with-results.json';
 
 function App() {
+  const movies = responseMovies.Search;
+  const hasMovies = movies?.length > 0;
   return (
-    <>
-      <h1>Prueba técnica</h1>
-    </>
+    <div className="page">
+      <header>
+        <h1>Buscador de peliculas</h1>
+        <form className="form">
+          <input type="text" placeholder="Avengers, Star Wars, The Matrix..." />
+          <button>Buscar</button>
+        </form>
+      </header>
+
+      <main>Resultados</main>
+    </div>
   );
 }
 
