71b35052eb0f04ad9de843aaca6ea8ece60317f2
Jesus Molina <netod90@gmail.com>
2023-09-06 18:57:07 -0700

    Add render mockup results

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -15,7 +15,21 @@ function App() {
         </form>
       </header>
 
-      <main>Resultados</main>
+      <main>
+        {hasMovies ? (
+          <ul>
+            {movies.map((movie) => (
+              <li key={movie.imdbID}>
+                <h3>{movie.Title}</h3>
+                <p>{movie.Year}</p>
+                <img src={movie.Poster} alt={movie.Title} />
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p>No se encontraron resultados</p>
+        )}
+      </main>
     </div>
   );
 }
