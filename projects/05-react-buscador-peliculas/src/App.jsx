953cda8d2d9206d7645c643f72e67161f6be575c
Jesus Molina <netod90@gmail.com>
2023-09-08 21:50:48 -0700

    Add handleSubmit

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,22 +1,37 @@
+import { useRef } from 'react';
 import './App.css';
 import { Movies } from './components/Movies';
 import { useMovies } from './hooks/useMovies';
 
 function App() {
-  const { movies: mappedMovies } = useMovies();
+  const { movies } = useMovies();
+  //const inputRef = useRef();
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //  const fields = new window.FormData(e.target);
+    //  const query = fields.get('query');
+    const { query } = Object.fromEntries(new window.FormData(e.target));
+    console.log(query);
+    //  const value = inputRef.current.value;
+    //  console.log(value);
+  };
   return (
     <div className="page">
       <header>
         <h1>Buscador de peliculas</h1>
-        <form className="form">
-          <input type="text" placeholder="Avengers, Star Wars, The Matrix..." />
+        <form className="form" onSubmit={handleSubmit}>
+          <input
+            name="query"
+            type="text"
+            placeholder="Avengers, Star Wars, The Matrix..."
+          />
           <button>Buscar</button>
         </form>
       </header>
 
       <main>
-        <Movies movies={mappedMovies} />
+        <Movies movies={movies} />
       </main>
     </div>
   );
