c16002946143ac232a693f06118a5c3823856482
Jesus Molina <netod90@gmail.com>
2023-09-14 18:03:44 -0700

    Add useSearch hook

--- projects/05-react-buscador-peliculas/src/App.jsx
+++ projects/05-react-buscador-peliculas/src/App.jsx
@@ -1,33 +1,66 @@
-import { useRef } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 import { Movies } from './components/Movies';
 import { useMovies } from './hooks/useMovies';
 
+function useSearch() {
+  const [search, updateSearch] = useState('');
+  const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
+
+  useEffect(() => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === '';
+      return;
+    }
+
+    if (search === '') {
+      setError('No se introdujo ningún titulo');
+      return;
+    }
+
+    setError(null);
+  }, [search]);
+
+  return { search, updateSearch, error };
+}
+
 function App() {
   const { movies } = useMovies();
+  const { search, updateSearch, error } = useSearch();
+
   //const inputRef = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    //  const fields = new window.FormData(e.target);
-    //  const query = fields.get('query');
-    const { query } = Object.fromEntries(new window.FormData(e.target));
-    console.log(query);
-    //  const value = inputRef.current.value;
-    //  console.log(value);
+    //const { query } = Object.fromEntries(new window.FormData(e.target));
+
+    console.log({ search });
   };
+
+  const handleChange = (e) => {
+    updateSearch(e.target.value);
+  };
+
   return (
     <div className="page">
       <header>
         <h1>Buscador de peliculas</h1>
         <form className="form" onSubmit={handleSubmit}>
           <input
+            style={{
+              border: '1px solid transparent',
+              borderColor: error ? 'red' : 'transparent',
+            }}
+            onChange={handleChange}
+            value={search}
             name="query"
             type="text"
             placeholder="Avengers, Star Wars, The Matrix..."
           />
           <button>Buscar</button>
         </form>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
       </header>
 
       <main>
