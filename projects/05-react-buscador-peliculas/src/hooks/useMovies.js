4364b8c713408a96f025df3c8466c8fc92e07db4
Jesus Molina <netod90@gmail.com>
2023-09-20 17:41:26 -0700

    Add service to fetch data/separate logic

--- projects/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,31 +1,13 @@
 import { useState } from 'react';
-import withResults from '../mocks/with-results.json';
-import withoutResults from '../mocks/no-results.json';
+import { searchMovies } from '../services/movies';
 
 export const useMovies = ({ search }) => {
-  const [responseMovies, setResponseMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
 
-  console.log(responseMovies.Search);
-  const movies = responseMovies.Search;
-  const mappedMovies = movies?.map((movie) => ({
-    id: movie.imdbID,
-    title: movie.Title,
-    year: movie.Year,
-    poster: movie.Poster,
-  }));
-
-  const getMovies = () => {
-    if (search) {
-      //  setResponseMovies(withResults);
-      fetch(`https://www.omdbapi.com/?apikey=4287ad07&s=${search}`)
-        .then((res) => res.json())
-        .then((json) => {
-          setResponseMovies(json);
-        });
-    } else {
-      setResponseMovies(withoutResults);
-    }
+  const getMovies = async () => {
+    const newMovies = await searchMovies({ search });
+    setMovies(newMovies);
   };
 
-  return { movies: mappedMovies, getMovies };
+  return { movies, getMovies };
 };
