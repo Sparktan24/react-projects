95bea39d5efdad6796ffa4f7f839d6980f5103d5
Jesus Molina <netod90@gmail.com>
2023-12-22 07:27:40 -0700

    Add filter logic

--- projects/06-shopping-cart/src/App.jsx
+++ projects/06-shopping-cart/src/App.jsx
@@ -1,10 +1,29 @@
+import { useState } from 'react';
+import { products as initialProducts } from './mocks/products.json';
 import Products from './components/Products';
-import { products } from './mocks/products.json';
+
 function App() {
+  const [products] = useState(initialProducts);
+  const [filters, setFilters] = useState({
+    category: 'all',
+    minPrice: 0,
+  });
+
+  const filterProducts = (products) => {
+    return products.filter((product) => {
+      return (
+        product.price >= filters.minPrice &&
+        (filters.category === 'all' || filters.category === product.category)
+      );
+    });
+  };
+
+  const filteredProducts = filterProducts(products);
+
   return (
     <>
       <h1>Shopping cart</h1>
-      <Products products={products} />
+      <Products products={filteredProducts} />
     </>
   );
 }
